bc8343bb21a493f2ad1021f75a77a5383b46643d
maxwellmattryan <maxwellmattryan@gmail.com>
2021-03-17 22:38:20 -0500

    Add env vars and support for mainnet interaction

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-const Iota = require('@iota/core');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const instance = Iota.composeAPI({
-    provider: 'https://nodes.devnet.iota.org:443'
+const Iota = require('@iota/core');
+const Converter = require('@iota/converter');
+
+const depth = 3;
+const minimumWeightMagnitude = 9;
+// CAUTION: The devnet requires a minimum weight magnitude of 9.
+
+const seed = process.env.WALLET_SEED
+const address = process.env.WALLET_ADDRESS;
+
+const message = JSON.stringify({'message': 'Hello, Tangle!'});
+const messageInTrytes = Converter.asciiToTrytes(message);
+
+const transfers = [{
+    value: 0.0,
+    address: address,
+    message: messageInTrytes
+}];
+
+const iota = Iota.composeAPI({
+    provider: process.env.NODE_URL
 });
 
-instance.getNodeInfo()
+console.log("Fetching node info...")
+iota.getNodeInfo()
 .then(info => console.log(JSON.stringify(info, null, 1)))
 .catch(err => {
-    console.log(err);
+    console.error(err);
+});
+
+console.log("Preparing Tangle transfers...")
+iota.prepareTransfers(seed, transfers)
+.then(trytes => {
+    return iota.sendTrytes(trytes, depth, minimumWeightMagnitude);
+})
+.then(bundle => {
+    console.log(bundle[0].hash);
+})
+.catch(err => {
+    console.error(err);
 });
