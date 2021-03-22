17acf7cd089f00b988abc9e436571cd4f932ace0
maxwellmattryan <maxwellmattryan@gmail.com>
2021-03-21 23:03:30 -0500

    Add basic files needed to get API service going

---
+++ .github/issue_template.md
@@ -0,0 +1,9 @@
+## Summary
+
+Provide an explanation for the issue along with any important or cautionary information. If possible try to provide direct documentation of the bug occuring.
+
+## Notes
+
+- [Note XXX]
+- [Note YYY]
+- [Note ZZZ]
---
+++ api/.eslintrc.js
@@ -0,0 +1,111 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
+    },
+    plugins: [
+        '@typescript-eslint',
+        'prettier'
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    root: false,
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        '@typescript-eslint/interface-name-prefix': [
+            'off'
+        ],
+        '@typescript-eslint/explicit-function-return-type': [
+            'error'
+        ],
+        '@typescript-eslint/explicit-module-boundary-types': [
+            'off'
+        ],
+        '@typescript-eslint/no-explicit-any': [
+            'error'
+        ],
+        '@typescript-eslint/no-return-await': [
+            'off'
+        ],
+        '@typescript-eslint/no-unused-vars': [
+            'warn'
+        ],
+        '@typescript-eslint/naming-convention': [
+            'error',
+            {
+                'selector': 'variable',
+                'format': [
+                    'camelCase',
+                    'UPPER_CASE'
+                ]
+            }
+        ],
+        "@typescript-eslint/no-array-constructor": [
+            "error"
+        ],
+        "@typescript-eslint/no-base-to-string": [
+            "error"
+        ],
+        "@typescript-eslint/no-dupe-class-members": [
+            "error"
+        ],
+        "@typescript-eslint/no-dynamic-delete": [
+            "off"
+        ],
+        "@typescript-eslint/no-empty-function": [
+            "off"
+        ],
+        "@typescript-eslint/no-empty-interface": [
+            "error"
+        ],
+        "@typescript-eslint/no-extra-non-null-assertion": [
+            "error"
+        ],
+        "@typescript-eslint/no-invalid-this": [
+            "error"
+        ],
+        "@typescript-eslint/no-invalid-void-type": [
+            "error"
+        ],
+        "@typescript-eslint/no-misused-new": [
+            "error"
+        ],
+        "@typescript-eslint/no-misused-promises": [
+            "error",
+            {
+                "checksVoidReturn": false
+            }
+        ],
+        "@typescript-eslint/no-require-imports": [
+            "error"
+        ],
+        "@typescript-eslint/no-throw-literal": [
+            "error"
+        ],
+        "@typescript-eslint/no-type-alias": [
+            "off"
+        ],
+        "@typescript-eslint/no-unnecessary-type-arguments": [
+            "error"
+        ],
+        "@typescript-eslint/no-unnecessary-type-assertion": [
+            "error"
+        ],
+        "@typescript-eslint/space-before-function-paren": [
+            "error",
+            {
+                "anonymous": "never",
+                "named": "never",
+                "asyncArrow": "always"
+            }
+        ],
+    },
+};
