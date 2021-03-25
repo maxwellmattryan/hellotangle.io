98c07fd974ecaa987a1481bc0f7f3b1f79510601
maxwellmattryan <maxwellmattryan@gmail.com>
2021-03-25 01:59:03 -0500

    Put in stubbed about page

---
+++ web/src/app/core/pages/about-page/about.page.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AboutPage } from './about.page';
+
+describe('AboutPageComponent', () => {
+    let component: AboutPage;
+    let fixture: ComponentFixture<AboutPage>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [AboutPage]
+        })
+            .compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AboutPage);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
